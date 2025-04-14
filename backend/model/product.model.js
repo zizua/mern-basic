4c7d07d702eabb0559c936653fd9518d7508677b
Zizu <Azizu066@gmail.com>
2025-04-14 11:36:41 +0700

    3. creating the product  model

---
+++ backend/model/product.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Automatically add createdAt and updatedAt fields
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+// Product = products
+
+export default Product;
