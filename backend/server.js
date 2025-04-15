e9fb6d58f6486d27a056f50cf347ed12eb22af89
Zizu <Azizu066@gmail.com>
2025-04-15 17:43:07 +0700

    5. create endpoint to delete a product

--- backend/server.js
+++ backend/server.js
@@ -13,23 +13,40 @@ const app = express();
 // });
 
 // 3. create endpoint to create a product
-app.post('/products', async (req, res) => {
-  const product = req.body; //user will this data
+app.use(express.json()); // allows us to accept JSON data in req.body
 
-  if (!product.name || product.price || product.image) {
-    return res.send(400).json({ success: false, message: 'Please fill all the fields' });
+app.post('/api/products', async (req, res) => {
+  const product = req.body; //user will send this data
+  if (!product.name || !product.price || !product.image) {
+    return res.status(500).json({ success: false, message: 'Please fill all the fields' });
   }
 
-  const newProduct = new Product(product); // create a new product object
-
+  const newProduct = new Product(product);
   try {
-    await newProduct.save(); // save the product to the database
-    res.send(201).json({ success: true, message: 'Product created successfully' });
+    await newProduct.save();
+    console.log('Product created successfully!');
+    res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
-    console.log('Error creating product:', error);
-    res.send(500).json({ success: false, message: 'Server error' });
+    console.error('Error creating product:', error.message);
+    res.status(500).json({ success: false, message: 'Server error' });
   }
 });
+
+// 4. create endpoint to delete a product
+app.delete('/api/products/:id', async (req, res) => {
+  const { id } = req.params; // get the id from the url
+
+  try {
+    await Product.findByIdAndDelete(id); // delete the product from the database
+    console.log(`Product id: ${id} successfully deleted!`);
+    res.status(200).json({ success: true, message: 'Product deleted successfully' });
+  } catch (error) {
+    console.log('Error deleting product:', error.message);
+    res.status(404).json({ success: false, message: 'Product not found' });
+  }
+});
+
+
 // console.log(process.env.MONGO_URI);
 
 // 1. run the server
